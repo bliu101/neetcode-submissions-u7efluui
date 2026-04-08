755afa80fb7c44ad091ab07370892599f205c3a3
Bridgette Liu <107775445+bliu101@users.noreply.github.com>
2026-04-08 13:01:05 -0400

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # return smallest k, so only search when right reset smaller
+        # bst on array 1 to max(piles)
+        minTime = 0
+        l, r = 1, max(piles)
+
+        while l <= r:
+            m = l + (r - l) // 2
+            totalTime = 0
+
+            for bananas in piles:
+                totalTime += math.ceil(bananas / m)
+            
+            if totalTime <= h:
+                minTime = m
+                r = m - 1
+            else: # eating time is larger than h, so need to increase eating amt
+                l = m + 1
+
+        return minTime
+
+
+
